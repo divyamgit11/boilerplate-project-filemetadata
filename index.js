41f787ef8dfab7de3e073de79187d1e2df53ea7d
divyamgit911 <divyam1071.be21@chitkarauniversity.edu.in>
2024-04-21 21:17:46 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+app.post('/api/fileanalyse', upload.single('upfile') ,(req,res)=>{
   res.json({
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
 });
 });
 
